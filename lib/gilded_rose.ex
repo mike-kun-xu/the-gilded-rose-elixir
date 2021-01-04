53415aaed924e0038e7677985858b1b368ea3a5b
mike-kun-xu <mike.kun.xu@gmail.com>
2021-01-04 12:36:52 -0500

    csv and ruled by category

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -2,22 +2,20 @@ defmodule GildedRose do
   use Agent
   alias GildedRose.Item
   alias GildedRose.Category, as: Cate
+  alias GildedRose.Date, as: Da
 
   # Data seeding when initialize the system
   def seed() do
-    {:ok, content} = File.read("seed/seed.csv")
-    File.write("data/item.csv",content)
-    cate_list = Seed.categorize(content)
-    Seed.writeFile(:category,cate_list)
-    date_list = Seed.setDate(content)
-    Seed.writeFile(:date,date_list)
+    {:ok, content} = File.read("priv/seed.csv")
+    :ok = File.write("priv/item.csv", content)
+    Seed.go()
   end
 
-# Retrieving data from database (using text file to simulate) and load into Agent.
+  # Retrieving data from database (using text file to simulate) and load into Agent.
   def new() do
-    {:ok, item_content} = File.read("data/item.csv")
-    {:ok, cate_content} = File.read("data/category.csv")
-    {:ok, date_content} = File.read("data/date.csv")
+    {:ok, item_content} = File.read("priv/item.csv")
+    {:ok, cate_content} = File.read("priv/category.csv")
+    {:ok, date_content} = File.read("priv/date.csv")
     item_content = String.split(item_content, "\n", trim: true)
     cate_content = String.split(cate_content, "\n", trim: true)
     date_content = String.split(date_content, "\n", trim: true)
@@ -28,141 +26,161 @@ defmodule GildedRose do
     agent
   end
 
-# Load data functionality.
+  # Load data functionality.
   defp loadItem([]), do: []
   defp loadItem(content) do
     item_to_load = hd(content) |> String.split("\,")
-    itemName = Enum.at(item_to_load,0)
-    {itemSellIn,_} = Integer.parse(Enum.at(item_to_load,1))
-    {itemQuality,_} = Integer.parse(Enum.at(item_to_load,2))
-    List.flatten([Item.new(itemName,itemSellIn,itemQuality),loadItem(tl(content))])
+    itemName = Enum.at(item_to_load, 0)
+    {itemSellIn, _} = Integer.parse(Enum.at(item_to_load, 1))
+    {itemQuality, _} = Integer.parse(Enum.at(item_to_load, 2))
+    List.flatten([Item.new(itemName, itemSellIn, itemQuality), loadItem(tl(content))])
   end
+
   defp loadCate([]), do: []
   defp loadCate(content) do
     item_to_load = hd(content) |> String.split("\,")
-    itemName = Enum.at(item_to_load,0)
-    itemCate = Enum.at(item_to_load,1)
-    List.flatten([Cate.new(itemName,itemCate),loadCate(tl(content))])
+    itemName = Enum.at(item_to_load, 0)
+    itemCate = Enum.at(item_to_load, 1)
+    List.flatten([Cate.new(itemName, itemCate), loadCate(tl(content))])
   end
+
   defp loadDate([]), do: []
   defp loadDate(content) do
     item_to_load = hd(content) |> String.split("\,")
-    itemName = Enum.at(item_to_load,0)
-    itemDate = Enum.at(item_to_load,1)
-    List.flatten([GildedRose.Date.new(itemName,itemDate),loadDate(tl(content))])
+    itemName = Enum.at(item_to_load, 0)
+    itemDate = Enum.at(item_to_load, 1)
+    List.flatten([Da.new(itemName, itemDate), loadDate(tl(content))])
   end
 
-# Get all items or get items from different map.
+  # Get all items or get items from different map.
   def items(agent), do: Agent.get(agent, & &1)
-  def items(agent, part) when part == :item do Agent.get(agent, & &1.item) end
-  def items(agent, part) when part == :cate do Agent.get(agent, & &1.cate) end
-  def items(agent, part) when part == :date do Agent.get(agent, & &1.date) end
+  def items(agent, :item) do
+    Agent.get(agent, & &1.item)
+  end
+  def items(agent, :cate) do
+    Agent.get(agent, & &1.cate)
+  end
+  def items(agent, :date) do
+    Agent.get(agent, & &1.date)
+  end
 
-
-# Update the quality and invoke the sell_in date and the data writing function.
+  # Update the quality and invoke the sell_in date and the data writing function.
   def update_quality(agent) do
-    item_num = GildedRose.items(agent,:item) |> length()
-    sell_list = update(agent,:item)
+    item_num = GildedRose.items(agent, :cate) |> length()
+
+    sell_list = update(agent, :item)
     Agent.update(agent, &Map.replace(&1, :item, sell_list))
-    quality_list = update_quality(agent,item_num,[])
+    quality_list = update_quality(agent, item_num, [])
     Agent.update(agent, &Map.replace(&1, :item, quality_list))
-    date_list = update(agent,:date)
+    date_list = update(agent, :date)
     Agent.update(agent, &Map.replace(&1, :date, date_list))
-    writeData("data/item.csv", agent)
-  end
-  defp update_quality(agent, 0, list), do: list
-  defp update_quality(agent, item_num, list) do
-    item = GildedRose.items(agent,:item) |> Enum.at(item_num - 1)
-    item = quality_change(item,item.name)
-    list = list ++ [item]
-    update_quality(agent, item_num - 1,list)  
+    :ok = writeData("priv/item.csv", agent)
   end
 
-# Pattern match different items.
-  defp quality_change(item,name) when name == "+5 Dexterity Vest" or name == "Elixir of the Mongoose" do
-    item =  
+  defp update_quality(_agent, 0, list), do: list
+
+  defp update_quality(agent, item_num, list) do
+    cate = items(agent, :cate) |> Enum.at(item_num - 1)
+    item = items(agent, :item) |> Enum.find(& &1.name == cate.name)
+    newItem = quality_change(item, String.to_atom(cate.category))
+    newList = List.flatten([list, newItem])
+    update_quality(agent,item_num - 1,newList)
+  end
+
+  # Pattern match different items.
+  defp quality_change(item, :normal) do
       if item.quality > 0 do
         if item.sell_in < 0 do
           %{item | quality: item.quality - 2}
         else
           %{item | quality: item.quality - 1}
         end
-      else 
-        item 
+      else
+        item
       end
   end
-  defp quality_change(item,name) when name == "Sulfuras Hand of Ragnaros" do
-    item
-  end
-  defp quality_change(item,name) when name == "Aged Brie" do
-    item =
+  defp quality_change(item, :legendary), do: item
+
+  defp quality_change(item, :reverse) do
       if item.quality < 50 do
         %{item | quality: item.quality + 1}
       else
         item
       end
   end
-  defp quality_change(item,name) when name == "Backstage passes to a TAFKAL80ETC concert" do
+  defp quality_change(item, :special) do
     item =
       cond do
-          item.sell_in <= 0 -> %{item | quality: 0}
-          item.sell_in <= 5 -> %{item | quality: item.quality + 3}
-          item.sell_in <= 10 -> %{item | quality: item.quality + 2}
-          true -> %{item | quality: item.quality + 1}
-        end
-        if item.quality > 50 do %{item | quality: 50} 
-        else 
-          item
-        end
+        item.sell_in <= 0 -> %{item | quality: 0}
+        item.sell_in <= 5 -> %{item | quality: item.quality + 3}
+        item.sell_in <= 10 -> %{item | quality: item.quality + 2}
+        true -> %{item | quality: item.quality + 1}
+      end
+    if item.quality > 50 do
+      %{item | quality: 50}
+    else
+      item
+    end
   end
-  defp quality_change(item,name) when name == "Conjured Mana Cake" do
-    item =
+  defp quality_change(item, :double) do
       if item.quality > 0 do
         if item.sell_in < 0 do
           %{item | quality: item.quality - 4}
         else
           %{item | quality: item.quality - 2}
         end
-      else 
-          item 
+      else
+        item
       end
   end
 
   # Update sell_in date
-  defp update(agent,part) when part == :item do 
-    update_sell_in(GildedRose.items(agent,:item))
-  end
-  defp update(agent,part) when part == :date do
-    update_date(GildedRose.items(agent,:date))
-  end
-  defp update_sell_in([]), do: []
-  defp update_sell_in(list=[head|tail]) do
-    if head.name == "Sulfuras Hand of Ragnaros" do
-      List.flatten([head,update_sell_in(tail)])
-    else
-      head = %{head|sell_in: head.sell_in - 1}
-      List.flatten([head,update_sell_in(tail)])
-    end
+  defp update(agent, :item) do
+    update_sell_in(items(agent, :item),agent)
   end
 
+  defp update(agent, :date) do
+    update_date(items(agent, :date))
+  end
+
+  defp update_sell_in([],_agent), do: []
+
+  defp update_sell_in([head | tail],agent) do
+    if head.name == Enum.find(items(agent,:cate),& &1.category == "legendary").name do
+      List.flatten([head, update_sell_in(tail, agent)])
+    else
+      head = %{head | sell_in: head.sell_in - 1}
+      List.flatten([head, update_sell_in(tail, agent)])
+    end
+  end
   defp update_date([]), do: []
-  defp update_date(list=[head|tail]) do
-    head = %{head|date: to_string(Date.utc_today())}
-    List.flatten([head,update_date(tail)])
+  defp update_date([head | tail]) do
+    head = %{head | date: to_string(Date.utc_today())}
+    List.flatten([head, update_date(tail)])
   end
 
   # Write updated data back to the text file.
   defp writeData(filename, agent) do
-    list = GildedRose.items(agent,:item)
-    data = write(list)
-    File.write(filename,data)
+    list = GildedRose.items(agent, :item)
+    File.write(filename, write(list))
   end
+
   defp write(list) when length(list) == 1 do
     item = hd(list)
-    List.flatten([item.name,",",to_string(item.sell_in),",",to_string(item.quality)])
+    List.flatten([item.name, ",", to_string(item.sell_in), ",", to_string(item.quality)])
   end
+
   defp write(list) do
     item = hd(list)
-    List.flatten([item.name,",",to_string(item.sell_in),",",to_string(item.quality),"\n",write(tl(list))])
+
+    List.flatten([
+      item.name,
+      ",",
+      to_string(item.sell_in),
+      ",",
+      to_string(item.quality),
+      "\n",
+      write(tl(list))
+    ])
   end
 end
--- lib/seed.exs
+++ lib/seed.exs
@@ -1,14 +1,18 @@
 defmodule Seed do
 alias GildedRose.Category, as: Cate
 
-# Called when seeding, create category data structure for each item
-  def categorize(content) do
-    item_to_load = String.split(content, "\n", trim: true)
-    categorize(item_to_load,[])
+# Called when seeding, create category and date structure for each item
+  def go() do
+    content = File.stream!("priv/seed.csv") |> CSV.decode
+    len = content |> Enum.count
+    {cate_list,date_list} = {categorize(content,len,[]),setDate(content,len,[])}
+    {:ok,:ok} = {writeFile(:category, cate_list),writeFile(:date, date_list)}
   end
-  defp categorize([],cate_list), do: cate_list
-  defp categorize(content,cate_list) do
-    itemName = hd(content) |> String.split("\,") |> Enum.at(0)
+
+  defp categorize(_content,0,cate_list), do: cate_list
+  defp categorize(content,len,cate_list) do
+    {:ok, item} = content |> Enum.at(len-1)
+    itemName = item |> Enum.at(0)
     itemCate =
     case itemName do
       "+5 Dexterity Vest" -> :normal
@@ -19,35 +23,31 @@ alias GildedRose.Category, as: Cate
       "Conjured Mana Cake" -> :double
     end
     cate_list = List.flatten([cate_list, Cate.new(itemName,itemCate)])
-    categorize(tl(content),cate_list)
+    categorize(content,len-1,cate_list)
   end
 
-# Called when seeding, create date data structure for each item
-  def setDate(content) do
-    item_to_load = String.split(content, "\n", trim: true)
-    setDate(item_to_load,[])      
-  end
-  defp setDate([],date_list), do: date_list
-  defp setDate(content,date_list) do
-    itemName = hd(content) |> String.split("\,") |> Enum.at(0)
+  defp setDate(_content,0,date_list), do: date_list
+  defp setDate(content,len,date_list) do
+    {:ok, item} = content |> Enum.at(len-1)
+    itemName = item |> Enum.at(0)
     date = Date.utc_today() |> to_string()
     date_list = List.flatten([date_list, GildedRose.Date.new(itemName,date)])
-    setDate(tl(content),date_list)
+    setDate(content,len-1,date_list)
   end
 
 # Write files into their own .csv, data will be retrieved from those .csv files instead of seed.csv
-  def writeFile(type,content) when type == :category do
+  defp writeFile(:category,content) do
     data_list = writeCate(content,[])
-    File.write("data/category.csv",data_list)
+    File.write("priv/category.csv",data_list)
   end
-  def writeFile(type,content) when type == :date do
+  defp writeFile(:date,content) do
     data_list = writeDate(content,[])
-    File.write("data/date.csv",data_list)
+    File.write("priv/date.csv",data_list)
   end
 
   defp writeCate(list,data_list) when length(list) == 1 do
     item = hd(list)
-    data_list = List.flatten(data_list,[item.name,",",to_string(item.category)])
+    List.flatten(data_list,[item.name,",",to_string(item.category)])
   end 
   defp writeCate(list,data_list) do
     item = hd(list)
@@ -56,7 +56,7 @@ alias GildedRose.Category, as: Cate
   end
   defp writeDate(list,data_list) when length(list) == 1 do 
     item = hd(list)
-    data_list = List.flatten(data_list,[item.name,",",to_string(item.date)])
+    List.flatten(data_list,[item.name,",",to_string(item.date)])
   end
   defp writeDate(list,data_list) do
     item = hd(list)
